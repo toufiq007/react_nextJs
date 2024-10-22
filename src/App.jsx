c25d7f19ad3d814c08e7a8cb4c610a101513ddbc
toufiq007 <t.limon2342@gmail.com>
2024-10-22 18:54:09 +0600

    simple todo app without reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
-import "./App.css";
-import Messenger from "./components/Messenger";
-import Scoreboard from "./components/Scoreboard";
+import { useState } from "react";
+import TaskList from "./components/TaskList";
+import { initialTasks } from "./data/data";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  const getNextId = (tasks) => {
+    const nextId = tasks.reduce((prev, current) =>
+      prev && prev.id > current.id ? prev.id : current.id
+    );
+    return nextId + 1;
+  };
+
+  // handle add
+
+  const handleAddTasks = (text) => {
+    setTasks((prev) => [
+      ...prev,
+      { id: crypto.randomUUID(), text, done: false },
+    ]);
+  };
+
+  // handle delete
+  const handleDelete = (id) => {
+    setTasks((prev) => prev.filter((task) => task.id !== id));
+  };
+
+  // handleChange
+  const handleChange = (updatedTask) => {
+    setTasks((prev) =>
+      prev.map((task) =>
+        task.id === updatedTask.id ? { ...updatedTask } : task
+      )
+    );
+  };
+
   return (
-    <>
-      {/* <Scoreboard /> */}
-      <Messenger />
-    </>
+    <div>
+      <h2>Prague Itinerary</h2>
+      <TaskList
+        addTaxk={handleAddTasks}
+        tasks={tasks}
+        handleDelete={handleDelete}
+        handleChange={handleChange}
+      />
+    </div>
   );
-}
+};
 
 export default App;
