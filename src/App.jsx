49a9d561179a6fc363df1cbc4d2fb146c9acca1f
toufiq007 <t.limon2342@gmail.com>
2024-10-25 00:45:23 +0600

    remove more files

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-/* eslint-disable no-unused-vars */
-import { useReducer, useState } from "react";
-import TaskList from "./components/TaskList";
-import { initialTasks } from "./data/data";
-import taskReducer from "./reducers/taskReducer";
-import { TaskContextProvider } from "./context/taskContext";
-
 const App = () => {
-  return (
-    <TaskContextProvider>
-      <div>
-        <h2>Prague Itinerary</h2>
-        <TaskList />
-      </div>
-    </TaskContextProvider>
-  );
+  return <>
+    
+  </>;
 };
 
 export default App;
--- src/data/data.js
+++
@@ -1,5 +0,0 @@
-export const initialTasks = [
-  { id: 0, text: "Watch kafka museme", done: true },
-  { id: 1, text: "Watch pupet show", done: false },
-  { id: 3, text: "Lemon wall pic", done: false },
-];
