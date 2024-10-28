b1f078f04492b2f7a807fde0ff2388cf83a568b9
toufiq007 <t.limon2342@gmail.com>
2024-10-29 00:21:58 +0600

    module 4.4 and 4.5 where i am learning the dead side of the manual dom manipulation in react by useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import Focus from "./components/Focus";
+import { useRef, useState } from "react";
 
 const App = () => {
+  const [show, setShow] = useState(true);
+  const refData = useRef(null);
+  // we can manipulate the dom element but be carefull to manipulate the dom element
+  // we must avoid the dom element by own as much as possible
+  // this is not the right way to manipulate the dom element
+  const handleRemove = () => {
+    refData.current.remove();
+  };
   return (
     <div>
-      <Focus />
+      <button onClick={() => setShow(!show)}>show data</button>
+      <button onClick={handleRemove}>remove data </button>
+      {show && <h2>hello world</h2>}
+
+      {/*  this way is not valid for manipulation the dom element */}
+      {/* <div
+        style={{ background: "red", width: "100px", height: "100px" }}
+        ref={refData}
+      ></div> */}
     </div>
   );
 };
