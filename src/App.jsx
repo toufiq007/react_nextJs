cf1a8212c79872df30fa713e6c9199a837e23fa5
toufiq007 <t.limon2342@gmail.com>
2024-10-29 00:44:34 +0600

    module 4.6 when we want to execute the render and ref synchronously then we should be using flushSync() higher order functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import { useRef, useState } from "react";
+import TodoList from "./components/Todo";
 
 const App = () => {
-  const [show, setShow] = useState(true);
-  const refData = useRef(null);
-  // we can manipulate the dom element but be carefull to manipulate the dom element
-  // we must avoid the dom element by own as much as possible
-  // this is not the right way to manipulate the dom element
-  const handleRemove = () => {
-    refData.current.remove();
-  };
   return (
     <div>
-      <button onClick={() => setShow(!show)}>show data</button>
-      <button onClick={handleRemove}>remove data </button>
-      {show && <h2>hello world</h2>}
-
-      {/*  this way is not valid for manipulation the dom element */}
-      {/* <div
-        style={{ background: "red", width: "100px", height: "100px" }}
-        ref={refData}
-      ></div> */}
+      <TodoList />
     </div>
   );
 };
