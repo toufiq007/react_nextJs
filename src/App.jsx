250eaedb24cd3de8d7335b4fd8db83292e17f14a
toufiq007 <t.limon2342@gmail.com>
2024-10-30 21:10:07 +0600

    a example where we are learning the sequence to useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Comments from "./components/Comments";
+import { useState } from "react";
+import FullExample from "./components/FullExample";
 
 const App = () => {
+  const [show, setShow] = useState(false);
   return (
     <div>
-      <h2>learning data fetching</h2>
-      <Comments postId={1} />
+      <h2>learning useEffect sequence</h2>
+      <button onClick={() => setShow(!show)}>
+        {show ? "mount" : "unmount"} component
+      </button>
+      {show && <hr />}
+      {show && <FullExample />}
     </div>
   );
 };
