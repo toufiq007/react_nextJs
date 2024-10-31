2449c297e4b2b3a59a3600c4b8a42f8b0f79ddb3
toufiq007 <t.limon2342@gmail.com>
2024-10-31 22:44:57 +0600

    example three where i learn where useEffect should not be used like updateState while props changing or resetting state during props changing

--- src/components/ExampleContainer.jsx
+++ src/components/ExampleContainer.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react";
-import Example2 from "./Example2";
+// import Example2 from "./Example2";
+// import Example from "./Example";
+import ExampleThree from "./ExampleThree";
+import { items1,items2 } from "../utils/data";
 
 const ExampleContainer = () => {
-  const [userId, setUserId] = useState(1);
+        // example one
+//   const [userId, setUserId] = useState(1);
+
+// example three
+const [items,setItems] = useState(items1)
+
   return (
     <div>
-      <button onClick={() => setUserId(userId === 1 ? 2 : 1)}>
+      {/* this is example one */}
+      {/* <Example /> */}
+      {/* this is example two */}
+      {/* <button onClick={() => setUserId(userId === 1 ? 2 : 1)}>
         Switch to profile {userId === 1 ? 2 : 1}{" "}
-      </button>
+      </button> */}
       {/* we can easily tell react that every changing of userId rerendered this component by specifying the userid */}
-      <Example2 userId={userId} key={userId} />
+      {/* <Example2 userId={userId} key={userId} /> */}
+
+      {/* this is example three */}
+
+      <button onClick={()=> setItems(items1)}>Switch to item 1</button>
+      <button onClick={()=> setItems(items2)}>Switch to item 2</button>
+      <ExampleThree items={items} />
     </div>
   );
 };
