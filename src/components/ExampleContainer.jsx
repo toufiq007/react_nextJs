428430c8ab798a533da10fb229758ec85a79f4cd
toufiq007 <t.limon2342@gmail.com>
2024-11-01 09:15:35 +0600

    example four where i learn that post request is not handling in useEffect

--- src/components/ExampleContainer.jsx
+++ src/components/ExampleContainer.jsx
@@ -1,32 +1,10 @@
-import { useState } from "react";
-// import Example2 from "./Example2";
-// import Example from "./Example";
-import ExampleThree from "./ExampleThree";
-import { items1,items2 } from "../utils/data";
+import ExampleFour from "./ExampleFour";
 
 const ExampleContainer = () => {
-        // example one
-//   const [userId, setUserId] = useState(1);
-
-// example three
-const [items,setItems] = useState(items1)
-
   return (
     <div>
-      {/* this is example one */}
-      {/* <Example /> */}
-      {/* this is example two */}
-      {/* <button onClick={() => setUserId(userId === 1 ? 2 : 1)}>
-        Switch to profile {userId === 1 ? 2 : 1}{" "}
-      </button> */}
-      {/* we can easily tell react that every changing of userId rerendered this component by specifying the userid */}
-      {/* <Example2 userId={userId} key={userId} /> */}
-
-      {/* this is example three */}
-
-      <button onClick={()=> setItems(items1)}>Switch to item 1</button>
-      <button onClick={()=> setItems(items2)}>Switch to item 2</button>
-      <ExampleThree items={items} />
+      {/* example four */}
+      <ExampleFour />
     </div>
   );
 };
