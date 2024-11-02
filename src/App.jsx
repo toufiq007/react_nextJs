5e35ae7063cddb382a352fef573d3033fa5f6dfb
toufiq007 <t.limon2342@gmail.com>
2024-11-02 15:33:31 +0600

    learning a new hook useEffectEvent which is a underConstructed hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,38 @@
 import { useState } from "react";
-import Chatroom from "./components/Chatroom";
-import { ServerContext } from "./context/ServerContext";
+import Page from "./components/Page";
+import { ShoppingCartContext } from "./context/shoppingContext";
+
+const items = [
+  {
+    id: 1,
+    title: "product 1",
+  },
+
+  {
+    id: 2,
+    title: "product 2",
+  },
+];
 
 const App = () => {
-  const [roomId, setRoomId] = useState(null);
-  const [showChatRoom, setShowChatRoom] = useState(true);
-  const [selectServerUrl, setSelectServerUrl] = useState(null);
-
+  const [page, setPage] = useState("/home");
+  const [cartItems, setCartItems] = useState(items);
+  const handlePageChange = () => {
+    setPage(page === "/home" ? "/cart" : "/home");
+  };
+  const handleAddProduct = () => {
+    setCartItems((prev) => [...prev, { id: 3, title: "product 3" }]);
+  };
   return (
     <div>
-      <div>
-        <button onClick={() => setShowChatRoom(!showChatRoom)}>
-          {showChatRoom ? "Hide chat room" : "Show chat room "}
-        </button>
-        <hr />
-      </div>
-
-      <div>
-        <label htmlFor="">select Server url</label>
-        <select
-          name=""
-          id=""
-          onChange={(e) => setSelectServerUrl(e.target.value)}
-        >
-          <option value="http://localhost:123">server 1</option>
-          <option value="http://localhost:456">server 2</option>
-          <option value="http://localhost:789">server 3</option>
-        </select>
-      </div>
-
-      <div>
-        <label htmlFor="">select chat room</label>
-        <select name="" id="" onChange={(e) => setRoomId(e.target.value)}>
-          <option value="general">general</option>
-          <option value="travel">travel</option>
-          <option value="sports">sports</option>
-        </select>
-      </div>
-
-      <ServerContext.Provider
-        value={{ defaultContext: "http://localhost/defaultContext" }}
-      >
-        {showChatRoom && <Chatroom roomId={roomId} selectServerUrl={selectServerUrl} />}
-      </ServerContext.Provider>
+      <ShoppingCartContext.Provider value={items}>
+        <Page
+          url={page}
+          onPageChange={handlePageChange}
+          cartItems={cartItems}
+          onAddProduct={handleAddProduct}
+        />
+      </ShoppingCartContext.Provider>
     </div>
   );
 };
---
+++ src/utils/logVisit.js
@@ -0,0 +1,5 @@
+export function logVisit(url, numberOfItems) {
+  console.log(
+    `the user is visited this /${url} and he/she has added ${numberOfItems} number of items in the cart`
+  );
+}
