53b8993620c8fc9afdb2f6d9f7e0dbfe1308d500
toufiq007 <t.limon2342@gmail.com>
2024-11-10 20:22:14 +0600

    setup our first react-router-dom project

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Root.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
--- src/utils/logVisit.js
+++
@@ -1,5 +0,0 @@
-export function logVisit(url, numberOfItems) {
-  console.log(
-    `the user is visited this /${url} and he/she has added ${numberOfItems} number of items in the cart`
-  );
-}
