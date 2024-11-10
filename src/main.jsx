db6688da83a1599845f6604e3468dd4dbc413317
toufiq007 <t.limon2342@gmail.com>
2024-11-10 20:27:39 +0600

    setup our error route and show error page when routes are not valid

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./Root.jsx";
+import ErrorPage from "./Error.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
