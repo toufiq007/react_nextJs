e26457f31745e45b08ea95d141a8b9de88a76537
toufiq007 <t.limon2342@gmail.com>
2024-11-12 01:44:07 +0600

    use active class to the link

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,4 +1,4 @@
-import { Form, Link, Outlet, useLoaderData } from "react-router-dom";
+import { Form, Link, NavLink, Outlet, useLoaderData } from "react-router-dom";
 
 export default function Root() {
   const { contacts } = useLoaderData();
@@ -28,7 +28,12 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) => {
+                      return isActive ? "active" : isPending ? "pending" : "";
+                    }}
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -37,7 +42,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
