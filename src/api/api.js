7a8f25e621f7db4835d5dc473b896cb674bc7a81
toufiq007 <t.limon2342@gmail.com>
2024-11-13 22:16:36 +0600

    making our first request interceptors where i am adding a token to our request.headers

--- src/api/api.js
+++ src/api/api.js
@@ -4,4 +4,18 @@ const api = axios.create({
   baseURL: `http://localhost:8000`,
 });
 
+const authorizationToken = "fdkfdkfdjfkdjfdkfjdfjdfdjejwekwjew";
+// making our request interseptors
+// in every request this interceptors will be used and headers get this token in request headers
+api.interceptors.request.use(
+  (config) => {
+    config.headers["Authorization"] = `Bearer ${authorizationToken}`;
+    console.log(config);
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
 export default api;
