cde123d987cefeb74e6a69aea27739b67f19d85b
toufiq007 <t.limon2342@gmail.com>
2024-11-14 19:22:20 +0600

    add a global error

--- src/forms/LoginForm.jsx
+++ src/forms/LoginForm.jsx
@@ -3,9 +3,26 @@ import Field from "../components/Field";
 import FieldSet from "../components/FieldSet";
 
 const LoginForm = () => {
-  const { register, handleSubmit,formState:{errors} } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm();
   const onSubmit = (data) => {
-    console.log(data);
+    const user = { email: "x@gmail.com", password: "12345678" };
+    const isFound =
+      data.email === user.email && data.password === user.password;
+
+    if (!isFound) {
+      // Set error for email field specifically
+      setError("formError", {
+        type: "manual",
+        message: `User with email ${data.email} is not valid`,
+      });
+    }
+
+    console.log(errors);
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -17,7 +34,9 @@ const LoginForm = () => {
             name="email"
             id="email"
             placeholder="enter email.."
-            className={`p-2 border box-border w-[300px] rounded-md border-gray-300 ${errors.email && 'border-3 border-red-500'}`}
+            className={`p-2 border box-border w-[300px] rounded-md border-gray-300 ${
+              errors.email && "border-3 border-red-500"
+            }`}
           />
         </Field>
 
@@ -34,9 +53,14 @@ const LoginForm = () => {
             name="password"
             id="password"
             placeholder="enter password.."
-            className={`p-2 border box-border w-[300px] rounded-md border-gray-300 ${errors.password && 'border-3 border-red-500'}`}
+            className={`p-2 border box-border w-[300px] rounded-md border-gray-300 ${
+              errors.password && "border-3 border-red-500"
+            }`}
           />
         </Field>
+        {/* {errors.root.random && (
+        )} */}
+        <div className="text-red-500 my-3">{errors.formError.message}</div>
         <Field>
           <button className="text-md text-white text-md cursor-pointer p-1 border rounded-lg hover:bg-purple-600 bg-purple-400  px-5">
             Login
