71ddf90bcdaf01a2f98918b212655fad180e251b
toufiq007 <t.limon2342@gmail.com>
2024-11-15 09:08:30 +0600

    fetch our singleProduct and render in jsx but curerntly fetching this with pass the hardcoded id

--- client-react-query/src/App.jsx
+++ client-react-query/src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
+import ProductDetails from './components/ProductDetails'
 import ProductList from './components/ProductList'
 
 function App() {
 
   return (
-    <>
+    <div className='flex gap-3'>
       <ProductList/>
-    </>
+      <ProductDetails id={1} />
+    </div>
   )
 }
 
