501a527deea6a77798f629995ec81ff82d23bcae
toufiq007 <t.limon2342@gmail.com>
2024-11-15 08:22:38 +0600

    react-query-devtools in the project

--- client-react-query/src/main.jsx
+++ client-react-query/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const client = new QueryClient();
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={client}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
