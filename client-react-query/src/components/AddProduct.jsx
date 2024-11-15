20b96475c1f5e94a4c0964176412e16f7ef1a874
toufiq007 <t.limon2342@gmail.com>
2024-11-15 18:01:43 +0600

    makes states for handling the formdata in addProductDetails page

--- client-react-query/src/components/AddProduct.jsx
+++ client-react-query/src/components/AddProduct.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
 const AddProduct = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    price: 0,
+    rating: 5,
+    thumbnail: "",
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value =
+      e.target.type === "number" ? e.target.valueAsNumber : e.target.value;
+    setFormData({ ...formData, [name]: value });
+  };
+
   const submitData = (e) => {
     e.preventDefault();
+    console.log(formData);
   };
   return (
     <div className="m-2 p-2 bg-gray-100 w-1/5 h-1/2">
@@ -11,11 +29,15 @@ const AddProduct = () => {
         <input
           type="text"
           name="title"
+          value={formData.title}
+          onChange={handleChange}
           className="my-2 border p-2 rounded"
           placeholder="Enter a product title"
         />
         <textarea
           name="description"
+          value={formData.description}
+          onChange={handleChange}
           className="my-2 border p-2 rounded"
           placeholder="Enter a product description"
         />
@@ -23,12 +45,16 @@ const AddProduct = () => {
         <input
           type="number"
           name="price"
+          value={formData.price}
+          onChange={handleChange}
           className="my-2 border p-2 rounded"
           placeholder="Enter a product price"
         />
         <input
           type="text"
           name="thumbnail"
+          value={formData.thumbnail}
+          onChange={handleChange}
           className="my-2 border p-2 rounded"
           placeholder="Enter a product thumbnail URL"
         />
