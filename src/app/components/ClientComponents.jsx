15724c52a44a391d7e857530bb6d22c6cae5c74c
toufiq007 <t.limon2342@gmail.com>
2024-11-30 21:23:44 +0600

    in nextjs by default every component is server component.if we want to create client component then explictly write 'use client' at the very top of the component and this component will be a client component

---
+++ src/app/components/ClientComponents.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ClientComponents = () => {
+  return (
+    <div>
+      <h2>this is the client components</h2>
+    </div>
+  );
+};
+
+export default ClientComponents;
---
+++ src/app/components/ServerComponents.jsx
@@ -0,0 +1,9 @@
+const ServerComponents = () => {
+  return (
+    <div>
+      <h2>this is the server components</h2>
+    </div>
+  );
+};
+
+export default ServerComponents;
