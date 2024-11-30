b8d17908d40954376a8332ef775f51053d63ae13
toufiq007 <t.limon2342@gmail.com>
2024-11-30 19:47:07 +0600

    created our first route in nextjs

---
+++ src/app/products/page.jsx
@@ -0,0 +1,14 @@
+const page = () => {
+  return (
+    <div>
+      <h2>products page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+        quis fugiat recusandae optio quia dolor ratione unde. Sapiente,
+        consequatur quidem.
+      </p>
+    </div>
+  );
+};
+
+export default page;
