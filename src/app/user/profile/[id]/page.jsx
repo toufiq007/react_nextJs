d1b904a3df0d04320e7af93ca58917e12a6c9bd4
toufiq007 <t.limon2342@gmail.com>
2024-11-30 20:11:39 +0600

    catch dynamic route id with params

--- src/app/user/profile/[id]/page.jsx
+++ src/app/user/profile/[id]/page.jsx
@@ -1,6 +1,8 @@
-const MyProfile = () => {
+const MyProfile = ({ params }) => {
+  console.log(params);
   return (
     <div>
+        <h2 className="text-3xl text-red-500">this is user id : {params.id}</h2>
       <h2> this is my profile page</h2>
     </div>
   );
