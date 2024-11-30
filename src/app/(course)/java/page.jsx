5b4b352d092e583a60726f7b4c64a174bb4fed4d
toufiq007 <t.limon2342@gmail.com>
2024-11-30 19:54:50 +0600

    when a folder name is look like (course) then it should not be used in the url page by means route grouping

---
+++ src/app/(course)/java/page.jsx
@@ -0,0 +1,9 @@
+const JavaPage = () => {
+  return (
+    <div>
+      <h2>java page</h2>
+    </div>
+  );
+};
+
+export default JavaPage;
---
+++ src/app/(course)/python/page.jsx
@@ -0,0 +1,9 @@
+const PythonPage = () => {
+  return (
+    <div>
+      <h2>this is python page</h2>
+    </div>
+  );
+};
+
+export default PythonPage;
